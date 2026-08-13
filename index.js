5d1b87b781850532706bac52fcdf4727fe0bfe4d
John Tork <johntork39@gmail.com>
2026-08-12 21:59:45 -0300

    Aula trocando o GAT para POST

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import express from 'express' //TYPE: module
 // CONST EXPRESS = require('express') //TYPE: commonjs
 
 const app = express()
+app.use(express.json())
 //inicializa o express - new
 
+//exercicio 01 GET
 app.get("/api/somar", (req, res) => {
-  const num1 = Number(req.query.num1)
-  const num2 = Number(req.query.num2)
+  const num1 = Number(req.params.num1)
+  const num2 = Number(req.params.num2)
+  res.send({ message: num1 + num2 })
+})
+
+//exercico 01 com POST
+
+app.post("/api/somar", (req, res) => {
+  const num1 = req.body.num1
+  const num2 = req.body.num2
   res.send({ message: num1 + num2 })
 
   //const - recebe um valor que não pode ser alterado =0 
@@ -14,7 +24,7 @@ app.get("/api/somar", (req, res) => {
   //var - recebe um valor que pode ser alterado (mas não é recomendado)
 })
 
-//exercio2
+//exercio2 COM GET
 
 app.get("/api/salario/:valor/:hora", (req, res) => {
   const valorGanho = Number(req.params.valor)
@@ -25,7 +35,17 @@ app.get("/api/salario/:valor/:hora", (req, res) => {
   res.send({ message: resultado })
 })
 
-//exercicio3
+//exercicio 02 COM POST
+app.post("/api/salario/:valor/:hora", (req, res) => {
+  const valorGanho = req.body.valor
+  const horaTrabalhadas = req.body.hora
+
+  const resultado = valorGanho * horaTrabalhadas
+
+  res.send({ message: resultado})
+})
+
+//exercicio 03 COM GET 
 
 app.get("/api/medpesos/:peso1/:peso2/:peso3/:peso4/:peso5", (req, res) => {
   const peso1 = Number(req.params.peso1)
@@ -37,8 +57,21 @@ app.get("/api/medpesos/:peso1/:peso2/:peso3/:peso4/:peso5", (req, res) => {
   const media = (peso1 + peso2 + peso3 + peso4 + peso5) / 5
 
   res.send({ message: media })
+  //http://localhost:3000/api/medpesos
 })
-//http://localhost:3000/api/medpesos
+
+//exercicio 03 COM POST
+
+app.post("/api/medpesos/:peso1/:peso2/:peso3/:peso4/:peso5", (req, res) => {
+  const peso1 = req.body.peso1
+  const peso2 = req.body.peso2
+  const peso3 = req.body.peso3
+  const peso4 = req.body.peso4
+  const peso5 = req.body.peso5
+
+  const media = (peso1 + peso2 + peso3 + peso4 + peso5) / 5
+  res.send({ message: media })
+})
 
 //exercio4
 app.get("/api/convgraus/:celsius", (req, res) => {
@@ -67,5 +100,5 @@ app.get("/api/:segundos", (req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+  console.log("Servidor na porta 3000")
 })
